08b553d95c42e08a0553a704aa5762fe4b50006c
yopiangga <yopigambyok@gmail.com>
2024-03-22 09:23:14 +0700

    predict and env

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-import base64
 from typing import Type
-from urllib import response
 from flask_cors import CORS, cross_origin
 from flask import Flask, request, jsonify, send_file
 import os
@@ -9,26 +7,27 @@ import time
 from io import BytesIO
 
 from tensorflow.keras.models import load_model
-from tensorflow.keras.applications.resnet50 import preprocess_input
-from tensorflow.keras.preprocessing import image
 
 from PIL import Image
-import matplotlib.pyplot as plt
 import numpy as np
-import mahotas as mh
 import tensorflow as tf
-import cv2
-import pandas as pd
 import pydicom
 
-dir_image = "/home/yopiangga/Documents/Kuliah/PA/riset/upload-file/uploads/jpg/"
-dir = ""
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
 app = Flask(__name__)
 CORS(app)
+
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-model = load_model(dir + 'model/1.1 Model VIT B16 WP 50E Dense 64.h5')
+dir_self = os.environ.get("DIR_SELF", default="true")
+dir_express = os.environ.get("DIR_EXPRESS", default="true")
+
+model_path = dir_self + "model/model_vit"
+jpg_path = dir_express + "uploads/medical-record/jpg/"
+segmented_path = dir_express + "uploads/medical-record/segmented/"
+
+model = load_model(model_path)
 
 @app.route('/')
 @cross_origin()
@@ -38,8 +37,7 @@ def home():
 @app.route('/prediction', methods=['POST'])
 @cross_origin()
 def prediction():
-
-    if 'file' not in request.files:
+    if 'image' not in request.files:
         return jsonify({'error': 'No file part'})
 
     file = request.files['image']
@@ -56,54 +54,23 @@ def prediction():
     rescaled_image = (np.maximum(im,0)/im.max()) * 255
     image = np.uint8(rescaled_image)
     image = Image.fromarray(image)
-    image.save(dir_image + name_image + '.jpg')
-
-    img_width, img_height = 384, 384
-
-    target_size = (img_width, img_height)
-    img = image.resize(target_size)
-
-    gray_image = image_to_grayscale(img)
-
-    blur_image = image_to_blur(gray_image)
-
-    threshold_image = image_to_threshold(blur_image)
-
-    morphological_image = image_to_morphological(threshold_image)
+    image = image.convert("RGB")
+    nameImage = str(time.time())
+    image.save(nameImage + '.jpg')
 
     image_array = tf.keras.preprocessing.image.img_to_array(image)
-    image_array = tf.expand_dims(image_array, 0)  # Add batch dimension
-    image_array /= 255.0  
-    
-    # Get the predicted class index
+    image_array = tf.keras.preprocessing.image.smart_resize(image_array, (384, 384))
+    image_array = tf.expand_dims(image_array, 0)
+    image_array /= 255.0
+
+    prediction = model.predict(image_array)
+
     predicted_class_index = np.argmax(prediction[0])
 
-    # Map the index to class labels (assuming you have a list of class labels)
-    class_labels = ["normal", "hemorrhagic", "ischemic"]  # Replace with your actual class labels
+    class_labels = ["normal", "hemorrhagic", "ischemic"]
     predicted_class_label = class_labels[predicted_class_index]
 
     return jsonify(predicted_class_label)
-
-def image_to_grayscale(img):
-    gray_image = img.convert("L")
-    return gray_image
-
-def image_to_blur(img, kernel_size=(5, 5), sigma=0):
-    image = np.array(img)
-    blurred_image = cv2.GaussianBlur(image, kernel_size, sigma)
-    return blurred_image
-
-def image_to_threshold(img, block_size=11):
-    image = np.array(img)
-    _, thresholded_image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-    return thresholded_image
-
-def image_to_morphological(img, kernel_size=3):
-    image = np.array(img)
-    kernel = np.ones((kernel_size, kernel_size), np.uint8)
-    opened_image = cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
-    return opened_image
-
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host='0.0.0.0')
